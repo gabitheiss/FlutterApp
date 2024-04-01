00ec4d36fed2d9dbe8e3adee89c2ebc01ae25558
Gabi <tgabrielatt@fastshop.com.br>
2024-04-01 10:46:48 -0300

    Ajuste condição para verificar lista vazia

--- lib/screens/FavoritesScreen.dart
+++ lib/screens/FavoritesScreen.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'HomeScreen.dart';
 
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return const Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.favorite, color: Colors.teal, size: 30),
-          SizedBox(height: 20),
-          Text('Você ainda não tem palavras favoritadas!',
-              style: TextStyle(fontSize: 18, color: Colors.teal))
-        ],
-      ));
-    }
 
     return Scaffold(
       appBar: AppBar(
@@ -30,26 +16,41 @@ class FavoritesScreen extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-        body: Center(
-          child: ListView(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(10),
-              ),
-              for (var pair in appState.favorites)
-                ListTile(
-                  leading: const Icon(Icons.favorite, color: Colors.teal),
-                  title: Text(pair.asPascalCase, style: const TextStyle(color: Colors.teal)),
-                  trailing: GestureDetector(
-                    onTap: () {
-                      appState.removeFavorite(pair);
-                    },
-                    child: const Icon(Icons.delete, color: Colors.red),
-                  ),
-                ),
-            ],
-          ),
+      body: Center(
+        child: appState.favorites.isEmpty
+            ? const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite, color: Colors.teal, size: 30),
+            SizedBox(height: 20),
+            Text(
+              'Você ainda não tem palavras favoritadas!',
+              style: TextStyle(fontSize: 18, color: Colors.teal),
+            ),
+          ],
         )
+            : ListView(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(10),
+            ),
+            for (var pair in appState.favorites)
+              ListTile(
+                leading: const Icon(Icons.favorite, color: Colors.teal),
+                title: Text(
+                  pair.asPascalCase,
+                  style: const TextStyle(color: Colors.teal),
+                ),
+                trailing: GestureDetector(
+                  onTap: () {
+                    appState.removeFavorite(pair);
+                  },
+                  child: const Icon(Icons.delete, color: Colors.red),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
